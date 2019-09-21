be8ba746ebd8451d8f255b14ed94714f6ce6c331
vincent VB <vincent.budianto@yahoo.co.id>
2019-09-21 16:09:35 +0700

    register improved

--- js/register.js
+++ js/register.js
@@ -1,37 +1,92 @@
-function validate()
-{
-    if (document.getElementById('password').value == document.getElementById('confirm-password').value)
-    {
+function unameValidate() {
+    let getData = new FormData(document.forms.registerForm); 
+    let request = new XMLHttpRequest();
+    request.open("POST", "php/validation.php", true);
+    request.send(getData);
+
+    request.onload = function() {
+        switch (request.response) {
+            case '401':
+                var unameInput = document.getElementById('username-input').value;
+                console.log(unameInput);
+                document.getElementById('false-username').style.color = 'red';
+                document.getElementById('false-username').innerHTML = 'Username +' 
+                unameInput + ' already existed! Please use another username.';
+                break;
+
+            case '402':
+                alert('Email already existed');
+                break;
+
+            case '403':
+                alert('Phone number already existed');
+                break;
+        }
+    }
+
+    // e.preventDefault();
+}
+
+function validate() {
+    if (document.getElementById('password').value == document.getElementById('confirm-password').value) {
         document.getElementById('confirm-password').style.borderColor = 'green';
         document.getElementById('confirm-password').style.borderWidth = '1.5px';
     }
-    else
-    {
+    else {
         document.getElementById('confirm-password').style.borderColor = 'red';
         document.getElementById('confirm-password').style.borderWidth = '1.5px';
     }
 }
 
-function getFileName()
-{
+function getFileName() {
     let filename = document.getElementById('browser-button').files[0].name;
     document.getElementById('profile-picture-name').value = filename;
 }
 
-function register(e)
-{
+function register(e) {
     let getData = new FormData(document.forms.registerForm); 
     let request = new XMLHttpRequest();
     request.open("POST", "php/register.php", true);
     request.send(getData);
-    e.preventDefault();
 
-    let resp = request.response;
+    request.onload = function() {
+        switch (request.response) {
+            case '200':
+                alert('Registration successful');
+                window.location.replace('login.html');
+                break;
 
-    if (resp == 200)
-    {
-        window.location.replace('login.html');
+            case '201':
+                alert('Registration failed');
+                break;
+
+            case '301':
+                alert('Invalid username');
+                break;
+
+            case '302':
+                alert('Invalid phone number');
+                break;
+
+            case '303':
+                alert('Profile picture must not be be empty');
+                break;
+
+            case '304':
+                alert('File must not be more than 2 MB');
+                break;
+
+            case '305':
+                alert('Invalid file type');
+                break;
+
+            case '501':
+                alert('Failed to upload profile picture');
+                break;
+        }
     }
+    
+    e.preventDefault();
 }
 
 document.getElementById('registerForm').addEventListener('submit', register);
--- php/register.php
+++ php/register.php
@@ -8,8 +8,7 @@ $directory = "../assets/profilePicture/";
 $errors = array();
 $fileType = array('image/jpg', 'image/jpeg', 'image/png', 'image/svg');
 
-if ($_POST)
-{
+if ($_POST) {
     // Get input data
     $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
     $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
@@ -22,76 +21,39 @@ if ($_POST)
     $token = password_hash($username, PASSWORD_DEFAULT);
 
     //Input data validation
-    if (!preg_match("/\w/", $username))
-    {
+    if (!preg_match("/\w/", $username)) {
+        echo 301;
         array_push($errors, "Username is invalid");
     }
 
-    if ((strlen((string)$phoneNumber) < 9) or (strlen((string) $phoneNumber) > 12))
-    {
+    if ((strlen((string)$phoneNumber) < 9) or (strlen((string) $phoneNumber) > 12)) {
+        echo 302;
         array_push($errors, "Phone number is invalid");
     }
 
-    if (empty($nameProfilePicture))
-    {
+    if (empty($nameProfilePicture)) {
+        echo 303;
         array_push($errors, "Profile picture can't be empty");
     }
-    else if ($sizeProfilePicture > 2000000)
-    {
+    else if ($sizeProfilePicture > 2000000) {
+        echo 304;
         array_push($errors, "File can't be more than 2 MB");
     }
-    else if (!in_array($typeProfilePicture, $fileType))
-    {
+    else if (!in_array($typeProfilePicture, $fileType)) {
+        echo 305;
         array_push($errors, "File type is invalid");
     }
 
-    // Preparing checkQuery
-    $checkQuery = "SELECT * FROM users WHERE (username = :username) OR (email = :email) OR (phoneNumber = :phoneNumber) LIMIT 1";
-    $stmt1 = $db->prepare($checkQuery);
-
-    // Bind checkQuery parameters
-    $params1 = array(
-        ":username" => $username,
-        ":email" => $email,
-        ":phoneNumber" => $phoneNumber
-    );
-
-    // Execute checkQuery
-    $stmt1->execute($params1);
-    
-    // Existing data validation
-    $result = $stmt1->fetch(PDO::FETCH_ASSOC);
-
-    if ($result)
-    {
-        if ($result['username'] === $username)
-        {
-            array_push($errors, "Username already exists");
-        }
-
-        if ($result['email'] === $email)
-        {
-            array_push($errors, "Email already exists");
-        }
-
-        if ($result['phoneNumber'] === $phoneNumber)
-        {
-            array_push($errors, "Phone number already exists");
-        }
+    if (move_uploaded_file($tempProfilePicture, $directory . $nameProfilePicture)) {
+        $profilePicture = $directory . $nameProfilePicture;
     }
-
-    if (move_uploaded_file($tempProfilePicture, $directory . $nameProfilePicture))
-    {
-        $profilePicture = $dir . $nameProfilePicture;
-    }
-    else
-    {
+    else {
+        echo 501;
         array_push($errors, "Failed to upload profile picture");
     }
 
     // Preparing query
-    if (count($errors) == 0)
-    {
+    if (count($errors) == 0) {
         // Preparing insertQuery
         $insertQuery = "INSERT INTO users (username, email, phoneNumber, password, profilePicture, token) VALUES (:username, :email, :phoneNumber, :password, :profilePicture, :token)";
         $stmt2 = $db->prepare($insertQuery);
@@ -110,11 +72,11 @@ if ($_POST)
         $registered = $stmt2->execute($params2);
 
         // Go to login page
-        if ($registered)
-        {
+        if ($registered) {
             echo 200;
+        }
+        else {
+            echo 201;
         }
     }
 }
-
-?>
