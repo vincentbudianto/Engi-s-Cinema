a568e97e66d97e9f5dc8d759f980b6040e740e5f
xion <nixonandhika28@gmail.com>
2019-09-21 19:21:26 +0700

    more ajax for register

--- js/register.js
+++ js/register.js
@@ -1,77 +1,119 @@
 function unameValidate() {
-    let getData = new FormData(document.forms.registerForm); 
-    let request = new XMLHttpRequest();
-    request.open("POST", "php/regValidation.php", true);
-    request.send(getData);
+    var uname = document.getElementById('username-input').value;
+    if (uname.length < 5 || uname.length > 16) {
+        document.getElementById('username-input').style.borderColor = 'red';
+        document.getElementById('username-input').style.borderWidth = '1.5px';
+        document.getElementById('false-username-msg').style.color = 'red';
+        document.getElementById('false-username-msg').innerHTML = 'Username must be between 5-16 in length';
+    } else if (!/^[a-zA-Z0-9_]{5,16}$/.test(uname)) {
+        document.getElementById('username-input').style.borderColor = 'red';
+        document.getElementById('username-input').style.borderWidth = '1.5px';
+        document.getElementById('false-username-msg').style.color = 'red';
+        document.getElementById('false-username-msg').innerHTML = 'Username can only be combination of characters, numbers, or underscore';
+    } else {
+        let getData = new FormData(document.forms.registerForm); 
+        let request = new XMLHttpRequest();
+        request.open("POST", "php/regValidation.php", true);
+        request.send(getData);
 
-    request.onload = function() {
-        switch (request.response.substr(-3)) {
-            case '401':
-                var unameInput = document.getElementById('username-input').value;
-                document.getElementById('username-input').style.borderColor = 'red';
-                document.getElementById('username-input').style.borderWidth = '1.5px';
-                document.getElementById('false-username-msg').style.color = 'red';
-                document.getElementById('false-username-msg').innerHTML = 'Username ' + unameInput + ' already exist! Please use another username.';
-                break;
+        request.onload = function() {
+            switch (request.response.substr(-3)) {
+                case '401':
+                    var unameInput = document.getElementById('username-input').value;
+                    document.getElementById('username-input').style.borderColor = 'red';
+                    document.getElementById('username-input').style.borderWidth = '1.5px';
+                    document.getElementById('false-username-msg').style.color = 'red';
+                    document.getElementById('false-username-msg').innerHTML = 'Username ' + unameInput + ' already exist! Please use another username.';
+                    break;
             
-            default :
-                document.getElementById('username-input').style.borderColor = 'green';
-                document.getElementById('username-input').style.borderWidth = '1.5px';
-                document.getElementById('false-username-msg').innerHTML = '';
-                break;
+                default :
+                    document.getElementById('username-input').style.borderColor = 'green';
+                    document.getElementById('username-input').style.borderWidth = '1.5px';
+                    document.getElementById('false-username-msg').innerHTML = '';
+                    break;
+            }
         }
     }
 }
 
 function emailValidate() {
-    let getData = new FormData(document.forms.registerForm); 
-    let request = new XMLHttpRequest();
-    request.open("POST", "php/regValidation.php", true);
-    request.send(getData);
+    var email = document.getElementById('email-input').value;
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        document.getElementById('email-input').style.borderColor = 'red';
+        document.getElementById('email-input').style.borderWidth = '1.5px';
+        document.getElementById('false-email-msg').style.color = 'red';
+        document.getElementById('false-email-msg').innerHTML = 'Please input a correct email format';
+    } else {
+        let getData = new FormData(document.forms.registerForm); 
+        let request = new XMLHttpRequest();
+        request.open("POST", "php/regValidation.php", true);
+        request.send(getData);
 
-    request.onload = function() {
-        switch (request.response.substr(-3)) {
-            case '402':
-                document.getElementById('email-input').style.borderColor = 'red';
-                document.getElementById('email-input').style.borderWidth = '1.5px';
-                document.getElementById('false-email-msg').style.color = 'red';
-                document.getElementById('false-email-msg').innerHTML = 'Email already existed';
-                break;
+        request.onload = function() {
+            switch (request.response.substr(-3)) {
+                case '402':
+                    document.getElementById('email-input').style.borderColor = 'red';
+                    document.getElementById('email-input').style.borderWidth = '1.5px';
+                    document.getElementById('false-email-msg').style.color = 'red';
+                    document.getElementById('false-email-msg').innerHTML = 'Email already existed';
+                    break;
 
-            default :
-                document.getElementById('email-input').style.borderColor = 'green';
-                document.getElementById('email-input').style.borderWidth = '1.5px';
-                document.getElementById('false-email-msg').innerHTML = '';
-                break;
+                default :
+                    document.getElementById('email-input').style.borderColor = 'green';
+                    document.getElementById('email-input').style.borderWidth = '1.5px';
+                    document.getElementById('false-email-msg').innerHTML = '';
+                    break;
+            }
         }
     }
+    
 }
 
 function phoneValidate() {
-    let getData = new FormData(document.forms.registerForm); 
-    let request = new XMLHttpRequest();
-    request.open("POST", "php/regValidation.php", true);
-    request.send(getData);
+    var phone = document.getElementById('phone-input').value;
+    if (phone.length < 9 || phone.length > 12) {
+        document.getElementById('phone-input').style.borderColor = 'red';
+        document.getElementById('phone-input').style.borderWidth = '1.5px';
+        document.getElementById('false-phone-msg').style.color = 'red';
+        document.getElementById('false-phone-msg').innerHTML = 'Phone number must be between 9-12 numbers';
+    } else {
+        let getData = new FormData(document.forms.registerForm); 
+        let request = new XMLHttpRequest();
+        request.open("POST", "php/regValidation.php", true);
+        request.send(getData);
 
-    request.onload = function() {
-        switch (request.response.substr(-3)) {
-            case '403':
-                document.getElementById('phone-input').style.borderColor = 'red';
-                document.getElementById('phone-input').style.borderWidth = '1.5px';
-                document.getElementById('false-phone-msg').style.color = 'red';
-                document.getElementById('false-phone-msg').innerHTML = 'Phone number already existed';
-                break;
+        request.onload = function() {
+            switch (request.response.substr(-3)) {
+                case '403':
+                    document.getElementById('phone-input').style.borderColor = 'red';
+                    document.getElementById('phone-input').style.borderWidth = '1.5px';
+                    document.getElementById('false-phone-msg').style.color = 'red';
+                    document.getElementById('false-phone-msg').innerHTML = 'Phone number already existed';
+                    break;
 
-            default : 
-                document.getElementById('phone-input').style.borderColor = 'green';
-                document.getElementById('phone-input').style.borderWidth = '1.5px';
-                document.getElementById('false-phone-msg').innerHTML = '';
-                break;
+                default : 
+                    document.getElementById('phone-input').style.borderColor = 'green';
+                    document.getElementById('phone-input').style.borderWidth = '1.5px';
+                    document.getElementById('false-phone-msg').innerHTML = '';
+                    break;
+            }
         }
     }
 }
 
 function passValidate() {
+    var pass = document.getElementById('password').value;
+    if (pass.length < 6 || pass.length > 16) {
+        document.getElementById('password').style.borderColor = 'red';
+        document.getElementById('password').style.borderWidth = '1.5px';
+        document.getElementById('false-password-msg').style.color = 'red';
+        document.getElementById('false-password-msg').innerHTML = 'Password must be between 6-12 in length';
+    } else {
+        document.getElementById('password').style.borderColor = '#ccc';
+        document.getElementById('password').style.borderWidth = '1px';
+        document.getElementById('false-password-msg').innerHTML = '';
+    }
+
     if (document.getElementById('password').value == document.getElementById('confirm-password').value) {
         document.getElementById('confirm-password').style.borderColor = 'green';
         document.getElementById('confirm-password').style.borderWidth = '1.5px';
@@ -96,6 +138,7 @@ function register(e) {
     request.onload = function() {
         switch (request.response.substr(-3)) {
             case '200':
+                alert('Registration successful');
                 window.location.replace('login.html');
                 break;
 
