4f0cb36cd92fc62eb4c144669219568cb4571ef7
vincent VB <vincent.budianto@yahoo.co.id>
2019-09-28 21:12:22 +0700

    added basic func js

---
+++ js/navFunction.js
@@ -0,0 +1,6 @@
+let userCookie = (document.cookie.match(/^(?:.*;)?\s*user\s*=\s*([^;]+)(?:.*)?$/) || [, null])[1];
+
+if (userCookie == null) {
+	window.location.replace('login.html');
+}
+
--- js/ticket.js
+++ js/ticket.js
@@ -1,9 +1,3 @@
-let userCookie = (document.cookie.match(/^(?:.*;)?\s*user\s*=\s*([^;]+)(?:.*)?$/)||[,null])[1];
-
-if (userCookie == null) {
-    window.location.replace('login.html');
-}
-
 function payment() {
     document.getElementById('modal').style.display = 'block';
 }
