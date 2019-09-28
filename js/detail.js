5af1d3371efd0d1acebc14a56ccc7ab9266a3cb1
vincent VB <vincent.budianto@yahoo.co.id>
2019-09-28 15:25:53 +0700

    change cookie search to regex

--- js/detail.js
+++ js/detail.js
@@ -1,4 +1,5 @@
-if (document.cookie == "") {
+userCookie = (document.cookie.match(/^(?:.*;)?\s*user\s*=\s*([^;]+)(?:.*)?$/)||[,null])[1];
+
+if (userCookie == null) {
     window.location.replace('login.html');
 }
-
--- js/search.js
+++ js/search.js
@@ -1,4 +1,6 @@
-if (document.cookie == "") {
+userCookie = (document.cookie.match(/^(?:.*;)?\s*user\s*=\s*([^;]+)(?:.*)?$/)||[,null])[1];
+
+if (userCookie == null) {
     window.location.replace('login.html');
 }
 
