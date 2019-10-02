3dd37a17f21c61dd03d692795553760290cfb3c2
xion <nixonandhika28@gmail.com>
2019-10-02 13:32:05 +0700

    movie schedule (still bugged)

--- php/getMovieSchedule.php
+++ php/getMovieSchedule.php
@@ -7,8 +7,9 @@ if ($_GET) {
     //Get movie ID
     $movieID = $_GET["id"];
 
-    $query = "SELECT scheduleDate, scheduleTime, seat FROM schedule WHERE (movieID = :movieID)
-    AND (scheduleDATE - CURRENT_DATE >= 0)";
+    // $query1 = "UPDATE schedule SET seat=seat-1 WHERE scheduleID";
+    $query = "SELECT scheduleDate, scheduleTime FROM schedule WHERE (movieID = :movieID) AND 
+    (scheduleDATE - CURRENT_DATE >= 0)";
     $stmt = $db->prepare($query);
 
     $params = array(
